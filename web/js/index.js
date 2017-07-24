215fa4126ad2a1b617136a72a1e0f8e31217faed
ilya.shikhaleev <ilya.shikhaleev@ispringsolutions.com>
2017-07-24 19:07:17 +0300

    Для мобильников вместо звука добавлена фраза "Meow"

--- web/js/index.js
+++ web/js/index.js
@@ -21,8 +21,12 @@ socket.onmessage = function (e) {
     const event = JSON.parse(data);
     console.log(event);
     if (event.event === "play") {
-        const audio = new Audio("audio/hihat.wav");
-        audio.play();
+        if (isMobile()) {
+            window.speechSynthesis.speak(new SpeechSynthesisUtterance('meow'));
+        } else {
+            const audio = new Audio("audio/hihat.wav");
+            audio.play();
+        }
     }
 };
 
@@ -35,3 +39,28 @@ button.onclick = () => {
     socket.send("play");
     return false;
 };
+
+
+function isMobile() {
+    let useragent = navigator.userAgent;
+
+    if (useragent.match(/Android/i)) {
+        return true;
+    } else if (useragent.match(/webOS/i)) {
+        return true;
+    } else if (useragent.match(/iPhone/i)) {
+        return true;
+    } else if (useragent.match(/iPod/i)) {
+        return true;
+    } else if (useragent.match(/iPad/i)) {
+        return true;
+    } else if (useragent.match(/Windows Phone/i)) {
+        return true;
+    } else if (useragent.match(/SymbianOS/i)) {
+        return true;
+    } else if (useragent.match(/RIM/i) || useragent.match(/BB/i)) {
+        return true;
+    } else {
+        return false;
+    }
+}
